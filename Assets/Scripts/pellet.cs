2bf0891918ce7b4b2f2edf857acdaffbafc5f233
Clinkman <59674587+Clinkman@users.noreply.github.com>
2021-02-04 18:49:11 -0500

    update

---
+++ Assets/Scripts/pellet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pellet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ScoreScript.scoreValue += 100;
+        Destroy(gameObject);
+    }
+}

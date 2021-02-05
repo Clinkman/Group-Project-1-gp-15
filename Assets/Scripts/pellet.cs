cedeb1602f74f1b65721545bf7dac4a91cb2d669
helling7904 <52261860+helling7904@users.noreply.github.com>
2021-02-05 12:18:34 -0500

    jkhg

--- Assets/Scripts/pellet.cs
+++ Assets/Scripts/pellet.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pellet : MonoBehaviour
 {
+    public Text Stext;
+    public int ScoreValue;
+    private void ScoreUpdate()
+    {
+
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         ScoreScript.scoreValue += 100;
         Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Stext = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Stext.text = "Score: " + ScoreValue;
     }
 }
